0a1856e49256c2fac59065d00c49101f889f138d
pwebb27 <p-webb27@hotmail.com>
2022-12-27 15:11:32 -0600

    chore: re-add Wakelock for troubleshooting

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Wakelock.enable();
+
     SystemChrome.setPreferredOrientations([
         DeviceOrientation.portraitUp,
         DeviceOrientation.portraitDown,

cb745cba56e4bf66cd2d407d23ff2f8fe731c91a
pwebb27 <p-webb27@hotmail.com>
2023-01-23 12:14:53 -0600

    feature: Add continous connectivity checking to application - Import connectivity_plus package - Integrate continual connectivity checking in main, displaying a snackbar upon network outage instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:crud_mobile_app/providers/DataEntryViewScreen/button_size_provider.dart';
 import 'package:crud_mobile_app/providers/DataEntryViewScreen/text_form_field_text_provider.dart';
 import 'package:crud_mobile_app/providers/DataEntryViewScreen/text_form_field_prefix_icon_color_provider.dart';
@@ -7,6 +9,7 @@ import 'package:wakelock/wakelock.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +23,67 @@ Future<void> main() async {
   ], child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
+  final _messangerKey = GlobalKey<ScaffoldMessengerState>();
+
+  late StreamSubscription<ConnectivityResult> subscription;
+
+  late ConnectivityResult? connectionStatus;
+
+  @override
+  void initState() {
+    subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      setState(() {
+        connectionStatus = result;
+        checkInternetConnectivity();
+      });
+    });
+    checkInitialConnectivity();
+    super.initState();
+  }
+
+  void checkInitialConnectivity() async {
+    await Connectivity().checkConnectivity().then((result) {
+      connectionStatus = result;
+      checkInternetConnectivity();
+    });
+  }
+
+  void checkInternetConnectivity() {
+    if (connectionStatus == ConnectivityResult.none ||
+        connectionStatus == null) {
+      _messangerKey.currentState!.showSnackBar(
+        SnackBar(
+          duration: Duration(days: 365),
+          backgroundColor: Colors.blue,
+          content: Row(
+            children: const [
+              Icon(Icons.signal_wifi_statusbar_connected_no_internet_4_sharp,
+                  color: Colors.white, size: 18),
+              SizedBox(width: 15),
+              Text(
+                'Network Offline',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      _messangerKey.currentState!.clearSnackBars();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +94,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
+        scaffoldMessengerKey: _messangerKey,
         title: 'Flutter Demo',
         theme: ThemeData(
             textTheme: const TextTheme(

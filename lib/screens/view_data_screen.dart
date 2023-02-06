9718f25e43b87aaeccbfea209b9fd0a29caf1753
pwebb27 <p-webb27@hotmail.com>
2023-02-06 17:43:34 -0600

    fix: Save state for ViewDataScreen when switching tabs

--- lib/screens/view_data_screen.dart
+++ lib/screens/view_data_screen.dart
@@ -4,24 +4,35 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
-class ViewDataScreen extends StatelessWidget {
+class ViewDataScreen extends StatefulWidget {
   const ViewDataScreen({super.key});
 
   @override
+  State<ViewDataScreen> createState() => _ViewDataScreenState();
+}
+
+class _ViewDataScreenState extends State<ViewDataScreen>
+    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.only(top: 5.0),
-            child: context.watch<PostsStreamProvider>().posts == null
-                ? const Center(child: CircularProgressIndicator())
-                : context.watch<PostsStreamProvider>().posts!.isEmpty
-                    ? const _NoPostsWidget()
-                    : ListView(reverse: true, children: [
-                        ...(context
-                            .watch<PostsStreamProvider>()
-                            .posts!
-                            .map((post) => _PostTile(post)))
-                      ])));
+      body: Padding(
+          padding: const EdgeInsets.only(top: 5.0),
+          child: context.watch<PostsStreamProvider>().posts == null
+              ? const Center(child: CircularProgressIndicator())
+              : context.watch<PostsStreamProvider>().posts!.isEmpty
+                  ? const _NoPostsWidget()
+                  : ListView(reverse: true, children: [
+                      ...(context
+                          .watch<PostsStreamProvider>()
+                          .posts!
+                          .map((post) => _PostTile(post)))
+                    ])),
+    );
   }
 }
 
@@ -59,54 +70,52 @@ class _PostTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 25),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      post.name,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(width: .1, color: Colors.grey.shade500),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.withOpacity(0.15),
-                          spreadRadius: 1,
-                          blurRadius: 15,
-                          offset: Offset(3, 2))
-                    ],
-                    color: Colors.grey.shade300,
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(12),
-                        bottomRight: Radius.circular(12),
-                        topRight: Radius.circular(12))),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    post.message,
-                    style: Theme.of(context).textTheme.bodyMedium,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    post.name,
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: .1, color: Colors.grey.shade500),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 1,
+                        blurRadius: 15,
+                        offset: Offset(3, 2))
+                  ],
+                  color: Colors.grey.shade300,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                      topRight: Radius.circular(12))),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  post.message,
+                  style: Theme.of(context).textTheme.bodyMedium,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, top: 4),
-                child: Text(
-                  DateFormat('MM/dd/yy').add_jm().format(post.timestamp),
-                  style: TextStyle(color: Colors.grey.shade500, fontSize: 9.5),
-                ),
-              )
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, top: 4),
+              child: Text(
+                DateFormat('MM/dd/yy').add_jm().format(post.timestamp),
+                style: TextStyle(color: Colors.grey.shade500, fontSize: 9.5),
+              ),
+            )
+          ],
         ),
       );
 }

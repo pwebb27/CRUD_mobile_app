69093eef3d1e29ab05754005a984a86ccc893b36
pwebb27 <p-webb27@hotmail.com>
2024-02-15 16:56:20 -0600

    refactor: Create repository class for post entity

---
+++ lib/features/domain/repositories/post_repository.dart
@@ -0,0 +1,7 @@
+import 'package:crud_mobile_app/core/failures.dart';
+import 'package:crud_mobile_app/features/data/models/post_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class PostRepository {
+  Future<Either<Failure, Post>> getPosts(int amountRequested);
+}

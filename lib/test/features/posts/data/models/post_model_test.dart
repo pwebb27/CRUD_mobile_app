3b289164b43217949d924388864842b17458782a
pwebb27 <parker.webb27@gmail.com>
2024-03-05 17:32:02 -0600

    test: create to and from Json post model tests

--- lib/test/features/posts/data/models/post_model_test.dart
+++ lib/test/features/posts/data/models/post_model_test.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:crud_mobile_app/features/data/models/post_model.dart';
 import 'package:crud_mobile_app/features/domain/entities/post.dart';
+import 'package:crud_mobile_app/test/fixtures/fixture_reader.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const tPostModel = PostModel('123', 'Test Name', 'Test message', 123);
+  const tPostModel = PostModel('1234', 'Test Name', 'Test message', 1234);
 
   test(
     'should be a subclass of Post entity',
@@ -15,4 +18,40 @@ void main() {
       // assert
     },
   );
+  group('fromJson', () {
+    test(
+      'should return a valid Post model',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('post.json'));
+        const testKey = '1234';
+
+        // act
+        final result =
+            PostModel.fromRealTimeDatabase(jsonPost: jsonMap, key: testKey);
+
+        expect(result, tPostModel);
+        // assert
+      },
+    );
+    group('toJson', () {
+      test(
+        'should return a JSON map containing the proper data',
+        () async {
+          // arrange
+          final result = tPostModel.toJson();
+          // act
+          final expectedMap = {
+            {
+              'key': '1234',
+              'name': 'Test Name',
+              'message': 'Test message',
+              'timestamp': '1234'
+            }
+          };
+          // assert
+        },
+      );
+    });
+  });
 }

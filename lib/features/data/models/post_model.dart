6acd895f1384c835bef79abcb2a23e490c413f9d
pwebb27 <parker.webb27@gmail.com>
2024-03-09 12:33:21 -0600

    refactor: Create required named parameters for PostModel class

--- lib/features/data/models/post_model.dart
+++ lib/features/data/models/post_model.dart
@@ -6,16 +6,27 @@ class PostModel extends Post {
   final int timestamp;
   final String key;
 
-  const PostModel(
-    this.key,
-    this.name,
-    this.message,
-    this.timestamp,
-  ) : super(key: key, name: key, message: message, timestamp: timestamp);
+  const PostModel({
+    required this.key,
+    required this.name,
+    required this.message,
+    required this.timestamp,
+  }) : super(key: key, name: key, message: message, timestamp: timestamp);
 
   factory PostModel.fromRealTimeDatabase(
       {required Map<dynamic, dynamic> jsonPost, required String key}) {
     return PostModel(
-        key, jsonPost['name'], jsonPost['message'], jsonPost['timestamp']);
+        key: key,
+        name: jsonPost['name'],
+        message: jsonPost['message'],
+        timestamp: jsonPost['timestamp']);
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'key': key,
+      'name': name,
+      'message': message,
+      'timestamp': timestamp
+    };
   }
 }

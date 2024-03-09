7c5fc87a29ee4b263c18da686a499922468e2c12
pwebb27 <parker.webb27@gmail.com>
2024-03-09 12:39:18 -0600

    refactor: Add members for PostRepositoryImp class with constructor

--- lib/features/data/repositories/post_repository_imp.dart
+++ lib/features/data/repositories/post_repository_imp.dart
@@ -1,12 +1,23 @@
 import 'package:crud_mobile_app/core/error/failures.dart';
+import 'package:crud_mobile_app/core/platform/network_info.dart';
+import 'package:crud_mobile_app/features/data/datasources/post_local_data_source.dart';
+import 'package:crud_mobile_app/features/data/datasources/post_remote_data_source.dart';
 import 'package:crud_mobile_app/features/domain/entities/post.dart';
 import 'package:crud_mobile_app/features/domain/repositories/post_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class PostRepositoryImpl implements PostRepository {
+  final PostRemoteDataSource remoteDataSource;
+  final PostLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  PostRepositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
   @override
   Future<Either<Failure, List<Post>>> getPosts(int? amountRequested) {
-    // TODO: implement getPosts
+    networkInfo.isConnected;
     throw UnimplementedError();
   }
 }

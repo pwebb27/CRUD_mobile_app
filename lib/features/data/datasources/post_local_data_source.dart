949d1fd985929fb15a63594ea4f7a6fd984eb4fe
pwebb27 <parker.webb27@gmail.com>
2024-03-23 12:54:33 -0500

    fix(PostLocalDataSource): Return Future<List<PostModel>> on getLastPosts() method instead of Future<List<Posts>>

--- lib/features/data/datasources/post_local_data_source.dart
+++ lib/features/data/datasources/post_local_data_source.dart
@@ -2,6 +2,6 @@ import 'package:crud_mobile_app/features/data/models/post_model.dart';
 import 'package:crud_mobile_app/features/domain/entities/post.dart';
 
 abstract class PostLocalDataSource {
-  Future<List<Post>> getLastPosts();
+  Future<List<PostModel>> getLastPosts();
   Future<void> cachePosts(List<PostModel> postsToCache);
 }

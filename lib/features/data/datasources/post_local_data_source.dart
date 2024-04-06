c5032a8012b0d24ccab3e91bdc840558b1c82d93
pwebb27 <parker.webb27@gmail.com>
2024-04-06 10:31:27 -0500

    refactor: Create outline for PostsLocalDataSouceImp class

--- lib/features/data/datasources/post_local_data_source.dart
+++ lib/features/data/datasources/post_local_data_source.dart
@@ -1,7 +1,28 @@
 import 'package:crud_mobile_app/features/data/models/post_model.dart';
 import 'package:crud_mobile_app/features/domain/entities/post.dart';
+import 'package:crud_mobile_app/test/features/posts/data/datasources/posts_local_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class PostLocalDataSource {
   Future<List<PostModel>> getLastPosts();
+
   Future<void> cachePosts(List<PostModel> postsToCache);
+}
+
+class PostsLocalDataSourceImp extends PostLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  PostsLocalDataSourceImp({required this.sharedPreferences});
+
+  @override
+  Future<void> cachePosts(List<PostModel> postsToCache) {
+    // TODO: implement cachePosts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<PostModel>> getLastPosts() {
+    // TODO: implement getLastPosts
+    throw UnimplementedError();
+  }
 }

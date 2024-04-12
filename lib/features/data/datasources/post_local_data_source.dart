ed182c7112054cb729dfc6e4fc5d1be88cdae411
pwebb27 <parker.webb27@gmail.com>
2024-04-12 12:23:10 -0500

    refactor: Create test and code for returning cache from sharedPreferences

--- lib/features/data/datasources/post_local_data_source.dart
+++ lib/features/data/datasources/post_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:crud_mobile_app/features/data/models/post_model.dart';
 import 'package:crud_mobile_app/features/domain/entities/post.dart';
 import 'package:crud_mobile_app/test/features/posts/data/datasources/posts_local_test.dart';
@@ -9,6 +11,8 @@ abstract class PostLocalDataSource {
   Future<void> cachePosts(List<PostModel> postsToCache);
 }
 
+const CACHED_POSTS = 'CACHED_POSTS';
+
 class PostsLocalDataSourceImp extends PostLocalDataSource {
   final SharedPreferences sharedPreferences;
 
@@ -16,13 +20,18 @@ class PostsLocalDataSourceImp extends PostLocalDataSource {
 
   @override
   Future<void> cachePosts(List<PostModel> postsToCache) {
-    // TODO: implement cachePosts
     throw UnimplementedError();
   }
 
   @override
   Future<List<PostModel>> getLastPosts() {
-    // TODO: implement getLastPosts
-    throw UnimplementedError();
+    final jsonString = sharedPreferences.getString(CACHED_POSTS);
+    List<dynamic> jsonPosts = json.decode(jsonString as String);
+    List<PostModel> postModels = [];
+    for (var post in jsonPosts) {
+      postModels.add((PostModel.fromRealTimeDatabase(
+          jsonPost: json.decode(post), key: '12345')));
+    }
+    return Future.value(postModels);
   }
 }
--- lib/test/features/posts/data/datasources/posts_local_test.dart
+++ lib/test/features/posts/data/datasources/posts_local_test.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:crud_mobile_app/features/data/datasources/post_local_data_source.dart';
+import 'package:crud_mobile_app/features/data/models/post_model.dart';
+import 'package:crud_mobile_app/test/fixtures/fixture_reader.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -12,6 +16,24 @@ class MockSharedPreferences extends Mock implements SharedPreferences {
       mockSharedPreferences = MockSharedPreferences();
       dataSource =
           PostsLocalDataSourceImp(sharedPreferences: mockSharedPreferences);
+    });
+    group('getLastPosts', () {
+      final tPostModel = PostModel.fromRealTimeDatabase(
+          jsonPost: json.decode(fixture('posts_cached.json')), key: '1234');
+      test(
+        'should return Posts from SharedPreferences when there is one in the cache',
+        () async {
+          // arrange
+          when(mockSharedPreferences.getString(any))
+              .thenReturn(fixture('posts_cached.json'));
+          // act
+          final result = await dataSource.getLastPosts();
+          // assert
+          verify(mockSharedPreferences.getString(CACHED_POSTS));
+          expect(result, equals(tPostModel));
+          verifyNoMoreInteractions(mockSharedPreferences);
+        },
+      );
     });
   }
 }
